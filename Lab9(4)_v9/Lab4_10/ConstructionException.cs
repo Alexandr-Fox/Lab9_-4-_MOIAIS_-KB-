11c1b48a09f15c2e6d22d1114f73c81fff0613d4
Alexandr-Fox <55092301+Alexandr-Fox@users.noreply.github.com>
2022-06-05 20:30:55 +0500

    Add files via upload
    Вариант с квадратом(зачтено Плотом)

---
+++ Lab9(4)_v9/Lab4_10/ConstructionException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab4_1
+{
+    public delegate void EventConstrPointHandler(Point point);
+    public class ConstructionException: Exception
+    {
+        public event EventConstrPointHandler EventConstr;
+        public void PrintEx(Point point)
+        {
+            Console.WriteLine($"Точка вызвавшая исключение: {point}");
+        }
+
+        public ConstructionException(Point point): base("По данным координатам можно построить только прямоугольник")
+        {
+            EventConstr += PrintEx;
+            EventConstr?.Invoke(point);
+        }
+    }
+}
---
+++ Lab9(4)_v9/Lab4_10/Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab4_1
+{
+    public class Point
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+        private readonly char _name;
+        public char Name { get => _name;
+            private init => _name = char.ToUpper(value); }
+
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+            Random random = new();
+            Name = (char)random.Next('A', 'Z' + 1);
+        }
+        public Point(double x, double y, char n)
+        {
+            X = x;
+            Y = y;
+            Name = n;
+        }
+        public override string ToString()
+        {
+            return $"{Name}({X},{Y})";
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
